dc03a2e038438575fad106d65a45bac06448c799
mukaazedo-glitch <mukaazedo@uni9.edu.br>
2026-05-19 02:57:18 +0000

    Entrega do projeto aluno

---
+++ Projeto-aluno/Controllers/AlunoController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_aluno.Controllers 
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Nome = "Seu Nome Completo";
+            ViewBag.Curso = "Análise e Desenvolvimento de Sistemas";
+            ViewBag.Semestre = "3º Semestre";
+
+            return View();
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            ViewBag.Id = id;
+            ViewBag.Nome = "Aluno Selecionado (ID: " + id + ")";
+            ViewBag.Curso = "Análise e Desenvolvimento de Sistemas";
+            ViewBag.Semestre = "3º Semestre";
+
+            return View();
+        }
+    }
+}
---
+++ Projeto-aluno/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_aluno.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
