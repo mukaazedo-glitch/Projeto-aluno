bb5cb3cd85a4dbc33ce832f87d9218e5ca056e02
mukaazedo-glitch <mukaazedo@uni9.edu.br>
2026-05-25 20:56:36 -0300

    Atualizar o AlunoController.cs

--- Projeto-aluno/Controllers/AlunoController.cs
+++ Projeto-aluno/Controllers/AlunoController.cs
@@ -18,7 +18,7 @@ namespace Projeto_aluno.Controllers
             ViewBag.Id = id;
             ViewBag.Nome = "Murilo" Selecionado (ID: " + id + ")";
             ViewBag.Curso = "Análise e Desenvolvimento de Sistemas";
-            ViewBag.Semestre = "3º Semestre";
+            ViewBag.Semestre = "1º Semestre";
 
             return View();
         }
